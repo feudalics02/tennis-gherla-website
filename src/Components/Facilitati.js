d1bfa1ddfdc93d3de956f02938ddd9e74bcca376
dragos.julean <dragos.julean@pitech.webhelp.com>
2025-03-05 23:18:42 +0200

    refactor: Simplify and update facilities list in Facilitati component

--- src/Components/Facilitati.js
+++ src/Components/Facilitati.js
@@ -11,19 +11,10 @@ export default function Facilitati() {
                             Clubul nostru de tenis oferă o gamă completă de facilități pentru toți iubitorii de tenis:
                         </p>
                         <ul className="list-disc list-inside space-y-2">
-                            <li>4 terenuri exterioare de tenis</li>
-                            <li>2 terenuri interioare pentru sezonul rece</li>
-                            <li>Vestiar modern cu dușuri</li>
+                            <li>2 terenuri exterioare de zgură</li>
+                            <li>1 teren interioare de hard</li>
+                            <li>Vestiar</li>
                             <li>Zonă de relaxare cu terasă</li>
-                            <li>Magazin de echipamente sportive</li>
-                            <li>Parcare privată</li>
-                        </ul>
-                    </div>
-                    <div className="space-y-4 text-gray-700">
-                        <p>
-                            Serviciile noastre includ:
-                        </p>
-                        <ul className="list-disc list-inside space-y-2">
                             <li>Antrenori profesioniști</li>
                             <li>Școală de tenis pentru copii și adulți</li>
                             <li>Închiriere echipament</li>
