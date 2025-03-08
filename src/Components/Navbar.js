394ec18e4eecec01f8d9528b9df364c63cbb0d32
dragos.julean <dragos.julean@pitech.webhelp.com>
2025-03-08 20:43:29 +0200

    Remove button from mobile menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -134,11 +134,6 @@ export default function Navbar() {
                                 {item.name}
                             </Link>
                         ))}
-                        <a
-                            href="tel:0743086789"
-                            className="w-full bg-primary text-white px-6 py-3 rounded-md hover:bg-secondary transition-colors duration-300 text-center block">
-                            Rezervă
-                        </a>
                     </div>
                 </div>
             )}
